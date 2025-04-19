3fec87d5b7b3f3a1046c71d0145fdb27f1797c67
Piotr Ruszkiewicz <piotrek007r@gmail.com>
2025-04-19 16:56:48 +0200

    make done

--- module0/ex01/repertory.hpp
+++ module0/ex01/repertory.hpp
@@ -3,16 +3,12 @@
 
 #include <string>
 #include <iostream>
-#include <cctype>
-#include <unistd.h>
 #include <iomanip>
+#include <cstdlib>
 
 typedef std::string str;
 
 #include "Contact.hpp"
-#include "Contact.cpp"
 #include "Phonebook.hpp"
-#include "Phonebook.cpp"
-
 
 #endif
