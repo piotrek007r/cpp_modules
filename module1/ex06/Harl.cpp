a60fb9a7c4c5ec64cd435b9ed04a35d75bf47247
Piotr Ruszkiewicz <pruszkie@c1r8s4.42warsaw.pl>
2025-06-07 19:21:16 +0200

    working on ex06

--- module1/ex06/Harl.cpp
+++ module1/ex06/Harl.cpp
@@ -2,43 +2,59 @@
 
 void Harl::debug(void)
 {
-    std::cout <<  
-        "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" 
-        << std::endl;
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout <<
+        "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+        << std::endl << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout <<  
-         "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" 
-        << std::endl;
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout <<
+         "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+        << std::endl << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout <<  
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout <<
          "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month."
-        << std::endl;
+        << std::endl << std::endl;
 }
 
 void Harl::error(void)
 {
-    std::cout <<  
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout <<
           "This is unacceptable! I want to speak to the manager now."
-        << std::endl;
+        << std::endl << std::endl;
+
 }
 
 void Harl::complain( std::string level )
 {
     std::string levelsArr[] = {"debug", "info", "warning", "error"};
-    void (Harl::*arrPtr[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    size_t i;
 
-    for(int i = 0; i <= 3; i++)
+    for(i = 0; i < 4; i++)
     {
         if(levelsArr[i] == level)
-        {
-            (this->*arrPtr[i])();
             break;
-        }
+    }
+    switch (i)
+    {
+        case 0:
+            this->debug();
+        case 1:
+            this->info();
+        case 2:
+            this->warning();
+        case 3:
+            this->error();
+        break;
+        default:
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
     }
 }
--- module1/ex06/main.cpp
+++ module1/ex06/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     {
         std::string level = argv[1];
         Harl Harl;
-
+        std::transform()
         Harl.complain(argv[1]);
     }
     else
@@ -16,23 +16,4 @@ int main(int argc, char **argv)
     }
 }
 
-//while(true)
-//{
-//    std::cout << std::endl;
-//    std::cout << "please type a comlain level: debug, info, warning, error, exit" << std::endl;
-//    std::cout << std::endl;
-//    std::getline(std::cin, level);
-//    if(level == "debug")
-//        Harl.complain("debug");
-//    else if(level == "info")
-//        Harl.complain("info");
-//    else if(level == "warning")
-//        Harl.complain("warning");
-//    else if(level == "error")
-//        Harl.complain("error");
-//    else if(level == "exit")
-//        break;
-//    else
-//        std::cout << "wrong level type, TRY AGAIN!" << std::endl;
 
-//}
