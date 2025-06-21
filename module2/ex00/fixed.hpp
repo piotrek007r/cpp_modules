6489f6c7b83dedb149ee5925cf2875203c21b0f4
Piotr Ruszkiewicz <piotrek007r@gmail.com>
2025-06-21 16:51:51 +0200

    m2 ex00 in progress

---
+++ module2/ex00/fixed.hpp
@@ -0,0 +1,51 @@
+#include "repertory.hpp"
+
+class Fixed
+{
+    public:
+        Fixed();
+        ~Fixed();
+
+        // now do a copy constructr
+        void setRawBits( int const raw );
+        int getRawBits( void );
+    private:
+        int fixedNum;
+        static const int fractNum = 8;
+};
+
+Fixed::Fixed()
+{
+    this->fixedNum = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() 
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->fixedNum = raw;
+}
+
+int Fixed::getRawBits( void )
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->fixedNum;
+}
+
+int main( void ) 
+{
+Fixed a;
+Fixed b( a );
+Fixed c;
+
+c = b;
+
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return 0;
+}
