4b5041b326dfbd0dc4e1511a73407e67575bf4ee
Piotr Ruszkiewicz <piotrek007r@gmail.com>
2025-06-22 15:43:51 +0200

    m2 ex00 done

--- module2/ex00/fixed.cpp
+++ module2/ex00/fixed.cpp
@@ -6,9 +6,18 @@ Fixed::Fixed()
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed&)
+Fixed::Fixed(const Fixed& other)
 {
     std::cout << "Copy constructor called" << std::endl;
+    this->fixedNum = other.getRawBits();
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->fixedNum = other.fixedNum;
+    return *this;
 }
 
 Fixed::~Fixed()
@@ -21,7 +30,7 @@ void Fixed::setRawBits(int const raw)
     this->fixedNum = raw;
 }
 
-int Fixed::getRawBits( void )
+int Fixed::getRawBits( void ) const
 {
     std::cout << "getRawBits member function called" << std::endl;
     return this->fixedNum;
--- module2/ex00/fixed.hpp
+++ module2/ex00/fixed.hpp
@@ -4,54 +4,15 @@ class Fixed
 {
     public:
         Fixed();
-        Fixed(const Fixed&);
+        Fixed(const Fixed& other);
+        Fixed& operator=(const Fixed& other);
         ~Fixed();
 
 
         void setRawBits( int const raw );
-        int getRawBits( void );
+        int getRawBits( void ) const;
     private:
         int fixedNum;
         static const int fractNum = 8;
 };
 
-//Fixed::Fixed()
-//{
-//    this->fixedNum = 0;
-//    std::cout << "Default constructor called" << std::endl;
-//}
-
-//Fixed::Fixed(const Fixed&)
-//{
-//    std::cout << "Copy constructor called" << std::endl;
-//}
-
-//Fixed::~Fixed()
-//{
-//    std::cout << "Destructor called" << std::endl;
-//}
-
-//void Fixed::setRawBits(int const raw)
-//{
-//    this->fixedNum = raw;
-//}
-
-//int Fixed::getRawBits( void )
-//{
-//    std::cout << "getRawBits member function called" << std::endl;
-//    return this->fixedNum;
-//}
-
-//int main( void )
-//{
-//Fixed a;
-//Fixed b( a );
-//Fixed c;
-
-//c = b;
-
-//std::cout << a.getRawBits() << std::endl;
-//std::cout << b.getRawBits() << std::endl;
-//std::cout << c.getRawBits() << std::endl;
-//return 0;
-//}
